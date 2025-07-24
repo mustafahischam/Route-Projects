b2b8e004e25f0b814505a95c248bd653a398c17e
Mustafa Hisham <57047334+mustafahischam@users.noreply.github.com>
2025-07-24 18:28:13 +0300

    Deploy Start-Framework build for GitHub Pages

--- Start-Framework/vite.config.js
+++ Start-Framework/vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/Route-Projects/Start-Framework/",
   plugins: [react()],
 });
